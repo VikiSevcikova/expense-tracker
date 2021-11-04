26d7855cb4c868764f95133c0574205386b1282f
kit486759 <kit486759@gmail.com>
2021-11-04 16:56:20 -0700

    nav

---
+++ src/pages/dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "../../features/navbar/Navbar";
+
+export default function Dashboard() {
+  return (
+    <div>
+      this is dashboard
+      <Nav />
+    </div>
+  );
+}
