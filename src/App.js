c44a8416468d16309a289a796dad475116cc0b0e
Yuki <yukipooh4120@gmail.com>
2021-11-11 19:53:45 -0800

    working on account page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./utils/main.scss";
 import Auth from "./pages/auth/Auth";
 import Dashboard from "./pages/dashboard/Dashboard";
 import AllTransaction from "./pages/allTransaction/AllTransaction";
+import Account from "./pages/account/Account";
 import PublicRoute from "./routes/PublicRoute";
 import PrivateRoute from "./routes/PrivateRoute";
 
@@ -11,21 +12,20 @@ const App = () => {
   return (
     <div className="App">
       <Router>
-          <Routes>
-            <Route element={<PublicRoute />}>
-              <Route path={"login"} element={<Auth />} />
-              <Route path={"registration"} element={<Auth />} />
-              <Route path={"forgot-password"} element={<Auth />} />
-              <Route path={"reset-password/:token"} element={<Auth />} />
-            </Route>
+        <Routes>
+          <Route element={<PublicRoute />}>
+            <Route path={"login"} element={<Auth />} />
+            <Route path={"registration"} element={<Auth />} />
+            <Route path={"forgot-password"} element={<Auth />} />
+            <Route path={"reset-password/:token"} element={<Auth />} />
+          </Route>
 
-            <Route element={<PrivateRoute />}>
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/alltransaction" element={<AllTransaction />} />
-            </Route>
-
-
-          </Routes>
+          <Route element={<PrivateRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/alltransaction" element={<AllTransaction />} />
+            <Route path="/account" element={<Account />} />
+          </Route>
+        </Routes>
       </Router>
     </div>
   );
