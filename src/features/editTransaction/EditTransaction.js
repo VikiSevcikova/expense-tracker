c8ba253dc29e6af4ce7d1ee62b40c33f5c5e8bc0
Yuki <yukipooh4120@gmail.com>
2021-11-15 11:00:42 -0800

    working on state control

--- src/features/editTransaction/EditTransaction.js
+++ src/features/editTransaction/EditTransaction.js
@@ -19,13 +19,40 @@ const EditTransaction = (props) => {
   console.log(props);//coming from filter.js
 
   //Calendar filter
-  const [startDate, setStartDate] = useState(new Date());
+  const [date, setDate] = useState(new Date());
+
+  //checkbox choices
+  const [trxType, setTrxType] = useState({})
+
+  //private state
+  const [transaction, setTransaction] = useState({
+    expense: true,
+    categoryName: "",
+    date: date,
+    amount: 0,
+    description: "",
+    paymentMethod: ""
+  });
+
+  //method
+  const handleChange = (prop) => (e) => {
+    console.log(prop);
+    console.log(e);
+    setTransaction({ ...transaction, [prop]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(transaction);
+
+    //validation check
+  };
 
   return (
     <>
       <Modal
         className="editModal"
-        show={props.show}
+        show={props.show} //coming from filter.js
         fullscreen
       >
         <Container fluid className="editTransactionContainer">
@@ -39,10 +66,24 @@ const EditTransaction = (props) => {
 
           {/* Body */}
           <Modal.Body>
-            <Form className="editForm">
-              <Form.Group className="transactionType">
-                <Form.Check type="radio" label="Income" />
-                <Form.Check type="radio" label="Expense" />
+            <Form
+              className="editForm"
+              onSubmit={handleSubmit}>
+              <Form.Group
+                className="transactionType"
+              >
+                <Form.Check
+                  type="radio"
+                  value="income"
+                  label="Income"
+                  name="income"
+                  onChange={handleChange("expense")} />
+                <Form.Check
+                  type="radio"
+                  value="expense"
+                  label="Expense"
+                  name="expense"
+                  onChange={handleChange("expense")} />
               </Form.Group>
 
               <Container fluid>
@@ -50,46 +91,63 @@ const EditTransaction = (props) => {
                   <Form.Label>Choose a Date *</Form.Label>
                   <DatePicker
                     required
-                    selected={startDate}
-                    onChange={(date) => setStartDate(date)} />
+                    selected={date}
+                    value={transaction.date}
+                    onChange={(date) => setDate(date)} />
                 </Form.Group>
 
                 <Form.Group className="transactionCategory">
                   <Form.Label>Select a Category *</Form.Label>
-                  <Form.Select required defaultValue="Choose...">
+                  <Form.Select
+                    required
+                    defaultValue="Choose..."
+                    value={transaction.categoryName}
+                    onChange={handleChange("categoryName")}>
                     <option>Choose...</option>
                     <option>Food</option>
-                    <option>Food</option>
-                    <option>Food</option>
-                    <option>Food</option>
+                    <option>House</option>
+                    <option>Transportation</option>
+                    <option>Other</option>
                   </Form.Select>
                 </Form.Group>
 
                 <Form.Group className="transactionAmount" >
                   <Form.Label>Enter an Amount *</Form.Label>
-                  <Form.Control required type="text" placeholder="$" />
+                  <Form.Control
+                    required type="text"
+                    placeholder="$"
+                    value={transaction.amount}
+                    onFocus={() => setTransaction({ ...transaction, ["amount"]: "" })}
+                    onChange={handleChange("amount")} />
                 </Form.Group>
 
                 <Form.Group className="transactionDescription" >
                   <Form.Label>Desctription</Form.Label>
-                  <Form.Control type="text" placeholder="Enter item description..." />
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter item description..."
+                    value={transaction.description}
+                    onChange={handleChange("description")} />
                 </Form.Group>
               </Container>
-              <Form.Group className="paymentMethod">
+
+              <Form.Group
+                className="paymentMethod"
+                value={transaction.paymentMethod}
+                onChange={handleChange("paymentMethod")}>
                 <Form.Label>Payment Method *</Form.Label>
                 <Form.Check type="radio" label="Debit Card" />
                 <Form.Check type="radio" label="Credit Card" />
                 <Form.Check type="radio" label="Cash" />
               </Form.Group>
 
+              <Container className="buttons">
+                <Button className="saveBtn" type="submit">Save</Button>
+                <Button className="deleteBtn">Delete</Button>
+              </Container>
+
             </Form>
           </Modal.Body>
-
-          {/* Footer */}
-          <Modal.Footer>
-            <Button className="saveBtn" type="submit">Save</Button>
-            <Button className="deleteBtn">Delete</Button>
-          </Modal.Footer>
         </Container>
       </Modal>
 
