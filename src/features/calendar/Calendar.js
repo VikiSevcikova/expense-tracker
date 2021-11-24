0b55fd04847293780338f5fa95e432adb5f9e7a8
Yuki <yukipooh4120@gmail.com>
2021-11-23 17:59:30 -0800

    calendar.js updated

--- src/features/calendar/Calendar.js
+++ src/features/calendar/Calendar.js
@@ -6,63 +6,76 @@ import moment from "moment";
 import { selectCalender } from "./calendarSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { calendarActions } from "../calendar/calendarSlice";
-import {getAllTransaction} from "../transactionList/transactionListSlice";
+import { getAllTransaction } from "../transactionList/transactionListSlice";
 import axios from "axios";
 import { balancePieChartActions } from "../balancePieChart/balancePieChartSlice";
 
 
-export default function Calendar({ setDate }) {
-  // const startOfMonth = new Date(moment().startOf("month"));
-  // const endOfDay = new Date(moment().endOf("day"));
-  // console.log(`Start Date ${startOfMonth}`);
-  // console.log(`End Date ${endOfDay}`);
+export default function Calendar() {
   const dispatch = useDispatch();
   const { startDate, endDate } = useSelector(selectCalender);
-console.log(startDate, endDate)
+  console.log(startDate, endDate);
   const [calendar, setCalendar] = useState([startDate, endDate]);
 
   const timeZoneOffSet = new Date().getTimezoneOffset() * 60000;
-  //const [startDate, endDate] = dateRange;
 
-  useEffect(() => {
-    //  set end date time
-   /* setDate(
-      startDate,
-      new Date(moment(endDate).set({ hour: 23, minute: 59, second: 59 }))
-    );*/
-    let dates = {startDate : calendar[0], endDate: calendar[1]}
+  useEffect(async () => {
+    let dates;
 
-    console.log(dates)
-   dispatch(calendarActions.setDateRange(dates));
-   const fetchDateRange = async () => {
-    try {
-      // Convert to ISO date format which is
-      const res = await axios.get(
-        `/transaction?startdate=${new Date(
-          calendar[0] - timeZoneOffSet
-        ).toISOString()}&enddate=${new Date(
-          calendar[1] - timeZoneOffSet
-        ).toISOString()}`
-      );
-      if (res.status === 200) {
-      //  dispatch(getAllTransaction(res.data));
-       dispatch(balancePieChartActions.getAmount(res.data));
-       console.log(res.data)
-        
-      }
-    } catch (err) {
-      return err;
+    //set end date to include 23:59:59
+    if (calendar[1] !== null) {
+      dates = {
+        startDate: calendar[0],
+        endDate: new Date(moment(calendar[1]).set({ hour: 23, minute: 59, second: 59 }))
+      };
+    } else {
+      dates = {
+        startDate: calendar[0],
+        endDate: calendar[1]
+      };
     }
-  };
-  fetchDateRange();
+
+    console.log("before dispatch!!", dates);
+
+    dispatch(calendarActions.setDateRange(dates));
+
+    const fetchDateRange = async () => {
+      try {
+        // Convert to ISO date format which is
+        const res = await axios.get(
+          `/transaction?startdate=${new Date(
+            calendar[0] - timeZoneOffSet
+          ).toISOString()}&enddate=${new Date(
+            calendar[1] - timeZoneOffSet
+          ).toISOString()}`
+        );
+        if (res.status === 200) {
+          //for transaction page
+          dispatch(getAllTransaction(res.data));
+
+          //for dashboard page
+          dispatch(balancePieChartActions.getAmount(res.data)); //pie chart
+
+          console.log(res.data);
+
+        }
+      } catch (err) {
+        return err;
+      }
+    };
+    fetchDateRange();
+
   }, [calendar]);
 
   return (
     <div className="calendar-wrapper">
       <DatePicker
         portalId="root-portal"
+        className="dateFilter"
+        dateFormat="yyyy/MM/dd"
+        monthsShown={2}
         selected={startDate}
-        onChange={(update) => setCalendar(update)}
+        onChange={update => setCalendar(update)}
         startDate={startDate}
         endDate={endDate}
         selectsRange={true}
