43480738c0dc86aa96e59c8e12e5513d2ebacc96
kit486759 <kit486759@gmail.com>
2021-11-30 00:55:18 -0800

    expense chart

--- src/features/expenseChart/ExpenseChart.js
+++ src/features/expenseChart/ExpenseChart.js
@@ -4,61 +4,98 @@ import useMedia from "use-media";
 
 export default function ExpenseChart({ allTran }) {
   const mobile = useMedia({ maxWidth: 767 });
-  const medium = useMedia({ maxWidth: 850 });
+  const medium = useMedia({ maxWidth: 878 });
 
   console.log(allTran);
 
-  const sortArray = [];
+  const dataList = [
+    { name: "Food & Beverage", trans: [] },
+    { name: "Shopping", trans: [] },
+    { name: "Grocery", trans: [] },
+    { name: "Utilities", trans: [] },
+    { name: "Transport & automobiles", trans: [] },
+    { name: "Insurance", trans: [] },
+    { name: "Medical", trans: [] },
+    { name: "Travel", trans: [] },
+    { name: "Housing", trans: [] },
+    { name: "Mobile", trans: [] },
+    { name: "Leisure", trans: [] },
+    { name: "Education", trans: [] },
+    { name: "Investment", trans: [] },
+    { name: "Other", trans: [] },
+    { name: "Salary", trans: [] },
+  ];
 
-  const test = allTran.filter((tran) => {
-    return tran.categoryName === "Education";
+  allTran.filter((tran) => {
+    //  return dataList.indexOf(tran.categoryName) > -1
+    dataList.forEach((list) => {
+      if (
+        list.name === tran.categoryName &&
+        tran.transactionType === "expense"
+      ) {
+        list.trans.push(tran);
+      }
+    });
   });
 
-  console.log(test);
+  // Filter out the lists if no transaction
+  const dataCatagory = dataList.filter((label) => {
+    return label.trans.length !== 0;
+  });
 
-  const size = {
-    desktop: 12,
-    device: 9,
-  };
+  // Get the list catagory label
+  const catagorylabel = dataCatagory.map((label) => {
+    return label.name;
+  });
 
-  // const dataList = [
-  //   { Foodbeverage: [] },
-  //   { Shopping: [] },
-  //   "Grocery",
-  //   "Utilities",
-  //   "Transport & automobiles",
-  //   "Insurance",
-  //   "Medical",
-  //   "Travel",
-  //   "Housing",
-  //   "Mobile",
-  //   "Leisure",
-  //   "Education",
-  //   "Investment",
-  //   "other",
-  // ];
+  // Get list catagory amount array
+  const catagoryAmount = dataCatagory.map((label) => {
+    const trans = label.trans.map((tran) => {
+      return tran.amount;
+    });
+    const sumTrans = trans.reduce((a, b) => {
+      return a + b;
+    });
+    return sumTrans;
+  });
 
-  const data = {
-    labels: [
-      "Food & beverage",
-      "Shopping",
-      "Grocery",
-      "Utilities",
-      "Transport & automobiles",
-      "Insurance",
-      "Medical",
-      "Travel",
-      "Housing",
-      "Mobile",
-      "Leisure",
-      "Education",
-      "Investment",
-      "other",
-    ],
+  // Total expense amount
+  const totalAmount = catagoryAmount.reduce((a, b) => a + b, 0);
+
+  console.log(totalAmount);
+  console.log(dataCatagory);
+  const chartAngle = catagoryAmount.map((amount) => {
+    return 360 * (amount / totalAmount);
+  });
+
+  console.log(catagoryAmount);
+  console.log(
+    catagoryAmount.map((amount) => {
+      return `$${amount}`;
+    })
+  );
+
+  const noData = {
+    labels: ["no data"],
     datasets: [
       {
-        label: "My First Dataset",
-        data: [50, 30, 60, 45, 70, 15, 5, 12, 7, 15, 10, 10, 20, 11],
+        labels: "no data",
+        data: [360],
+        backgroundColor: ["#191a24c8"],
+        borderWidth: 0,
+        font: {
+          size: 15,
+        },
+      },
+    ],
+  };
+
+  const data = {
+    labels: catagorylabel,
+    datasets: [
+      {
+        labels: catagorylabel,
+        data: catagoryAmount,
         backgroundColor: [
           "#a1bfa3",
           "#78a1a3",
@@ -91,7 +128,7 @@ export default function ExpenseChart({ allTran }) {
           font: {
             family: "Josefin Sans, sans-serif",
             // wordWrap: "break-word",
-            size: 12,
+            size: 18,
           },
         },
       },
@@ -134,14 +171,45 @@ export default function ExpenseChart({ allTran }) {
     },
   };
 
+  const nodataConfig = {
+    plugins: {
+      title: {
+        display: true,
+        text: "No transaction records this date range",
+        color: "white",
+        font: {
+          size: 16,
+        },
+      },
+
+      legend: {
+        display: false,
+        position: "bottom",
+
+        labels: {
+          color: "white",
+
+          font: {
+            family: "Josefin Sans, sans-serif",
+            size: 12,
+          },
+        },
+      },
+    },
+  };
+
   return (
     <div>
       <h5>ExpenseChart</h5>
       <div>
-        <Doughnut
-          data={data}
-          options={mobile ? mobileConfig : medium ? config : desktopConfig}
-        />
+        {data.labels.length !== 0 ? (
+          <Doughnut
+            data={data}
+            options={mobile ? mobileConfig : medium ? config : desktopConfig}
+          />
+        ) : (
+          <Doughnut data={noData} options={nodataConfig} />
+        )}
       </div>
     </div>
   );
