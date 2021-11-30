ecc910c9c65b169ef989f864f304feb40f78a1e2
Viki Sevcikova <viki.sevcikova63@gmail.com>
2021-11-29 16:46:43 -0800

    added input field for name on account page

--- src/features/userProfile/UserProfile.js
+++ src/features/userProfile/UserProfile.js
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
 import "./UserProfile.scss";
 import {
-  Container,
   Card,
   Button,
-  Modal,
+  Row,
+  Form,
 } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { selectUser } from './userSlice';
 import EditUser from "../editUser/EditUser";
 import DeleteConfirmation from '../deleteConfimation/DeleteConfirmation';
+import { MdEdit } from 'react-icons/md';
+import InputField from '../inputField/InputField';
+import { BsCheckCircleFill } from 'react-icons/bs';
+import * as Yup from "yup";
+import { Formik } from 'formik';
+
+const NameSchema = Yup.object().shape({
+  name: Yup.string().required("Name is required"),
+});
 
 const UserProfile = () => {
 
@@ -26,6 +35,7 @@ const UserProfile = () => {
   const closeDelConf = () => setShowDelConf(false);
   const showDelConf = () => setShowDelConf(true);
 
+  const [isEdit, setIsEdit] = useState(false);
 
   //method
   const changeUserAccount = (userInfo) => {
@@ -33,10 +43,54 @@ const UserProfile = () => {
     setShow(true);
   };
 
+  const changeIsEdit = () => {
+    setIsEdit(!isEdit);
+  }
+
+  const handleSubmit = (values) => {
+    console.log(values)
+    if(values.name !== user.username) {
+      console.log("changed")
+    }
+    changeIsEdit();
+  }
+
   return (
     <>
       <Card fluid className="userProfileCard">
-        <Card.Title>{user.username}</Card.Title>
+
+          <Card.Title className="d-flex justify-content-center mb-3">
+        {!isEdit ? 
+            
+            <>
+            {user.username} 
+            <MdEdit className="ms-2" onClick={changeIsEdit}/> 
+            </>
+            :
+            <Formik
+            initialValues={{
+              name: user.username,
+            }}
+            validationSchema={NameSchema}
+            onSubmit={(values) => {
+              handleSubmit(values);
+            }}
+          >
+          {({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => (
+            <>
+            <Form className="d-flex justify-content-center align-items-center">
+              <input id="name" type="text" className="input-name" value={values.name} onChange={handleChange} onBlur={handleBlur}/>
+              <BsCheckCircleFill onClick={(e)=>{e.preventDefault(); handleSubmit()}} fontSize="30px" className="ms-2 position-relative"/>
+            </Form>
+            <Form.Control.Feedback type="invalid" className="mb-3 ms-2 position-absolute">
+              {touched.name && errors.name}
+            </Form.Control.Feedback>
+            </>
+          )}
+          </Formik>
+        }
+         </Card.Title>
+
         <Card.Img className="avatar" src={avatar} />
         <Card.Body>
           <Button
