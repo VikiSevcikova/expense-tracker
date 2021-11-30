ff5b5d53c244c7a3ed6d658b8a69a45c54eaa484
Yuki <yukipooh4120@gmail.com>
2021-11-29 19:57:02 -0800

    search transaction feature done

--- src/features/search/SearchBar.js
+++ src/features/search/SearchBar.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from "react-redux";
 import {
   InputGroup,
   FormControl,
@@ -6,18 +7,40 @@ import {
 } from 'react-bootstrap';
 import "./SearchBar.scss";
 import { BsSearch } from "react-icons/bs";
-
+import { transactionListSelector, filterTransaction } from '../transactionList/transactionListSlice';
 
 const SearchBar = () => {
+
+  //private state
+  const [keyword, setKeyword] = useState("");
+
+  //redux
+  const transactionList = useSelector(transactionListSelector);
+  const dispatch = useDispatch();
+
+  //method
+  const searchTransaction = () => {
+    const filteredTranList = transactionList.allTran.filter(elem => {
+      if (elem.description.toLowerCase().includes(keyword.toLowerCase()) || elem.categoryName.toLowerCase().includes(keyword.toLowerCase())) {
+        return elem;
+      }
+    });
+    dispatch(filterTransaction(filteredTranList));
+  };
+
   return (
     <>
       <InputGroup className="searchBar">
         <FormControl
           className="searchInput"
           placeholder=" Search transaction item here..."
+          value={keyword}
+          // onFocus={setKeyword("")}
+          onChange={(e) => setKeyword(e.target.value)}
         />
         <Button
           className="searchBtn"
+          onClick={searchTransaction}
         >
           <BsSearch className="searchIcon" />
         </Button>
