19312ff14c00f2ccba0b03791dab981e51639fc1
Yuki <yukipooh4120@gmail.com>
2021-11-30 23:46:41 -0800

    edit prof pic completed

--- src/features/googleLogin/GoogleLoginBtn.js
+++ src/features/googleLogin/GoogleLoginBtn.js
@@ -49,8 +49,6 @@ const GoogleLoginBtn = () => {
           }, 5000);
     }
 
-    console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID)
-
     return (
         <GoogleLogin
             clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
